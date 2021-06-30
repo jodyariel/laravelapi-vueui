179627a190fa04f1468766630824914e6520a9de
jodyariel <72368409+jodyariel@users.noreply.github.com>
2021-06-30 17:03:22 +0700

    tugas 9
    tugas 9

--- src/views/friends/Createfriends.vue
+++ src/views/friends/Createfriends.vue
@@ -14,9 +14,9 @@
   <div class="col-md-6">
     <label for="inputPassword4" class="form-label">No Tlp</label>
     <input type="number" class="form-control" id="inputPassword4"
-    v-model="friend.no_tlp"/>
-    <div class="alert alert-danger" v-if="validation.no_tlp">
-        {{ validation.no_tlp[0] }}
+    v-model="friend.no_telp"/>
+    <div class="alert alert-danger" v-if="validation.no_telp">
+        {{ validation.no_telp[0] }}
       </div>
   </div>
   <div class="col-12">
@@ -37,33 +37,37 @@
  
 </template>
 <script>
-import { reactive, ref } from 'vue';
-import { useRouter } from 'vue-router';
-import axios from 'axios';
+import { ref } from 'vue';
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router'
+import axios from 'axios'
 export default {
   setup() {
+
     const friend = reactive({
       nama: '',
-      no_tlp: '',
-      alamat: '',
-    });
-    const validation = ref([]);
-    const router = useRouter();
+      no_telp: '',
+      alamat: ''
+    })
+
+    const validation = ref([])
+
+    const router = useRouter()
+
     function store(){
-      let nama = friend.nama;
-      let no_tlp = friend.no_tlp;
-      let alamat = friend.alamat;
-      axios.post("http://pia.labirin.co.id/api/friends/", {
+      let nama = friend.nama
+      let no_telp = friend.no_telp
+      let alamat = friend.alamat
+
+      axios.post('http://127.0.0.1:8000/api/friends', {
         nama: nama,
-        no_tlp: no_tlp,
-        alamat: alamat,
-      })
-      .then(() => {
+        no_telp: no_telp,
+        alamat: alamat
+      }).then(() => {
         router.push({
-          name:'Home',
-        });
-      })
-      .catch(error => {
+          name:'Home'
+        })
+      }).catch(error => {
         console.log(error)
       })
     }
@@ -71,7 +75,7 @@ export default {
       friend,
       validation,
       router, 
-      store,
+      store
     }
   },
 }
